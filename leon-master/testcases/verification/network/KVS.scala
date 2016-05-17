b0ca3e5b08e2ae8ee849dfb2659139292a9a2313
Alexandre Debant <debant@laraserver3.epfl.ch>
2016-05-17 09:33:15 +0200

    correction sytaxe

--- leon-master/testcases/verification/network/KVS.scala
+++ leon-master/testcases/verification/network/KVS.scala
@@ -6,7 +6,7 @@ import Networking._
 import sun.font.TrueTypeFont
 
 import scala.language.postfixOps
-
+import leon.lang._
 
 object Protocol {
   import ProtocolProof._
@@ -16,11 +16,13 @@ object Protocol {
   case class Read(s: String) extends Message
   case class Value(v: Option[BigInt]) extends Message
   case class Deliver(j: BigInt) extends Message
+  case class Increment() extends Message
 
-  case class State() extends State
+  case class CommonState() extends State
   case class BadState() extends State
 
-
+  case class ActorId1() extends ActorId
+  case class ActorId2() extends ActorId
   case class ActorIdSys(i: BigInt) extends ActorId
   case class ActorIdUser(i: BigInt) extends ActorId
 
@@ -35,7 +37,7 @@ object Protocol {
   case class SystemActor(myId: ActorId) extends Actor {
     require(myId == ActorIdSys(1) || myId == ActorIdSys(2) || myId == ActorIdSys(3))
 
-    var memory: MMap[String, BigInt] = MMap((x: String) => None[BigInt]())
+    var memory: MMap[String, BigInt] = MMap((x: String) => (None[BigInt]))
 
     def init()(implicit net: VerifiedNetwork) = {
       require(true)
@@ -46,23 +48,23 @@ object Protocol {
       require(true)
 
       (sender, m, state) match {
-        case (id, WriteUser(s,i), State()) =>
+        case (id, WriteUser(s,i), CommonState()) =>
           memory = memory.updated(s,i)
           if(myId != actor1){
-            !! (actor1, WriteSystem(s,i), State())
+            !! (actor1, WriteSystem(s,i))
           }
           if(myId != actor2){
-            !! (actor2, WriteSystem(s,i), State())
+            !! (actor2, WriteSystem(s,i))
           }
           if(myId != actor3){
-            !! (actor3, WriteSystem(s,i), State())
+            !! (actor3, WriteSystem(s,i))
           }
 
-        case (id, WriteSystem(s,i), State()) =>
+        case (id, WriteSystem(s,i), CommonState()) =>
           memory = memory.updated(s,i)
 
-        case (id,Read(s), State()) =>
-          !! (id, memory.getOrElse(s,None[BigInt]), State())
+        case (id,Read(s), CommonState()) =>
+          !! (id, Value(Some(memory.getOrElse(s,0)))) //cannot return None in default case
 
         case _ => update(BadState())
       }
@@ -81,10 +83,10 @@ object Protocol {
 
     def receive(sender: ActorId, m: Message)(implicit net: VerifiedNetwork) = {
       require(true)
-      check(sender == ActorIdSys(1) || sender == ActorIdSys(2) || sender == ActorIdSys(3))
+      check(sender == actor1 || sender == actor2 || sender == actor3)
 
       (sender, m, state) match {
-        case (sender, Value(v), State()) =>
+        case (sender, Value(v), CommonState()) =>
           x = v
           v match {
             case Some(i) => PrettyPrinting.messageToString(Deliver(i))
